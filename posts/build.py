2861dc45fb2fd1defee3a708cc6ba6d18065c5c5
vv4t <watashidesu6969@gmail.com>
2022-12-05 03:34:39 +1100

    public and private post setup

--- posts/build.py
+++ posts/build.py
@@ -1,4 +1,6 @@
 import sys
+import urllib.parse
+import re
 
 if len(sys.argv) != 3:
   print("usage: build [html] [shell]")
@@ -6,17 +8,16 @@ if len(sys.argv) != 3:
 
 html_file = open(sys.argv[1], "r")
 shell_file = open(sys.argv[2], "r")
-md_file = open(sys.argv[1].replace("html", "md"), "r")
 
 html_content = html_file.read()
 shell_content = shell_file.read()
-md_content = md_file.read()
 
-title = md_content.split("\n")[0].replace("# ", "")
+str_title = html_content.split("\n")[0]
+title = re.match("<h1 id=\"[a-z-0-9]+\">(.+)<\/h1>", str_title).group(1)
 
 output_html = shell_content.replace("{{{POST}}}", html_content).replace("{{{TITLE}}}", title)
+output_html = urllib.parse.unquote_plus(output_html)
 
-md_file.close()
 html_file.close()
 shell_file.close()
 
