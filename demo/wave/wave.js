1370c81705a849b59c3695550ad20c96745688b4
vv4t <watashidesu6969@gmail.com>
2022-12-10 05:26:42 +1100

    wave demo

---
+++ demo/wave/wave.js
@@ -0,0 +1,87 @@
+"use strict";
+
+import { pen_t } from "../wire-3d/pen.js";
+import { input_t, key } from "../wire-3d/input.js";
+import { clamp, vec2_t, vec3_t } from "../wire-3d/math.js";
+
+const canvas = document.getElementById("canvas");
+const pen = new pen_t(canvas);
+const input = new input_t(canvas);
+
+class bob_t {
+  constructor(u, u_t)
+  {
+    this.u = u;
+    this.u_t = u_t;
+  }
+};
+
+const BOB_SIZE = 0.025;
+const BOB_NUM = 100;
+const bob_arr = [];
+
+const bob_centre = BOB_NUM * BOB_SIZE * 0.5;
+
+function reset_bob() {
+  bob_arr.length = 0;
+  for (let i = 0; i < BOB_NUM; i++)
+    bob_arr.push(new bob_t(0.0, 0.0));
+}
+
+reset_bob();
+
+const TIMESTEP = 0.015;
+
+let t = 0.0;
+let stop = false;
+
+document.getElementById("stop").addEventListener("click", () => {
+  stop = true;
+});
+
+document.getElementById("start").addEventListener("click", () => {
+  stop = false;
+});
+
+document.getElementById("reset").addEventListener("click", reset_bob);
+
+const c_range = document.getElementById("c");
+
+setInterval(function() {
+  t += TIMESTEP;
+  
+  if (input.get_mouse_down()) {
+    const mouse_pos = input.get_mouse_pos();
+    const bob_num = clamp(Math.floor((mouse_pos.x + bob_centre) / BOB_SIZE), 1, bob_arr.length - 2);
+    bob_arr[bob_num].u = mouse_pos.y;
+  }
+  
+  pen.clear();
+  pen.begin();
+  
+  const c = c_range.value;
+  
+  if (!stop) {
+    for (let i = 1; i < bob_arr.length - 1; i++) {
+      const du_dx_1 = bob_arr[i + 1].u - bob_arr[i].u;
+      const du_dx_2 = bob_arr[i].u - bob_arr[i - 1].u;
+      
+      const d2u_dx2 = du_dx_1 - du_dx_2;
+      
+      bob_arr[i].u_t += c * d2u_dx2 * TIMESTEP;
+    }
+    
+    for (const bob of bob_arr)
+      bob.u += bob.u_t * TIMESTEP;
+  }
+  
+  for (let i = 0; i < bob_arr.length - 1; i++) {
+    const bob_a = new vec2_t(i * BOB_SIZE - bob_centre, bob_arr[i].u);
+    const bob_b = new vec2_t((i + 1) * BOB_SIZE - bob_centre, bob_arr[i + 1].u);
+    
+    pen.circle(bob_a, BOB_SIZE * 0.5);
+    pen.line(bob_a, bob_b);
+  }
+
+  pen.stroke();
+}, TIMESTEP * 1000);
---
+++ demo/wave/wave2d.js
@@ -0,0 +1,258 @@
+"use strict";
+
+import { pen_t } from "../wire-3d/pen.js";
+import { pen3d_t } from "../wire-3d/pen3d.js";
+import { input_t, key } from "../wire-3d/input.js";
+import { rand, clamp, vec2_t, vec3_t } from "../wire-3d/math.js";
+import { camera_t } from "../wire-3d/camera.js";
+
+const canvas = document.getElementById("canvas");
+const camera = new camera_t(new vec3_t(), new vec3_t());
+const pen = new pen_t(canvas);
+const pen3d = new pen3d_t(pen, camera);
+const input = new input_t(canvas);
+
+const TIMESTEP = 0.015;
+const BOB_NUM = 50;
+const RADIUS = 1.0;
+
+const c_range = document.getElementById("c");
+
+let stop = false;
+
+class bob_t {
+  constructor(u, u_t)
+  {
+    this.u = u;
+    this.u_t = u_t;
+  }
+};
+
+const sphere_arr = [];
+const bob_arr = [];
+
+function get_C()
+{
+  return c_range.value * 0.5;
+}
+
+function start()
+{
+  reset();
+}
+
+function update()
+{
+  pen.clear();
+  pen.begin();
+  
+  free_move();
+  free_look();
+  
+  for (const sphere of sphere_arr)
+    sphere.update();
+  
+  if (input.get_key(key.code(" "))) {
+    for (let i = -2; i <= 2; i++) {
+      for (let j = -2; j <= 2; j++) {
+        const x = clamp(Math.floor(camera.pos.x + i), 1, BOB_NUM - 2);
+        const y = clamp(Math.floor(camera.pos.z + j), 1, BOB_NUM - 2);
+        
+        bob_arr[y][x].u = camera.pos.y;
+      }
+    }
+  }
+  
+  if (!stop) {
+    for (let y = 1; y < BOB_NUM - 1; y++) {
+      for (let x = 1; x < BOB_NUM - 1; x++) {
+        const du_dx_1 = bob_arr[y][x].u - bob_arr[y][x - 1].u;
+        const du_dy_1 = bob_arr[y][x].u - bob_arr[y - 1][x].u;
+        
+        const du_dx_2 = bob_arr[y][x + 1].u - bob_arr[y][x].u;
+        const du_dy_2 = bob_arr[y + 1][x].u - bob_arr[y][x].u;
+        
+        const d2u_dx2 = du_dx_2 - du_dx_1;
+        const d2u_dy2 = du_dy_2 - du_dy_1;
+        
+        bob_arr[y][x].u_t += get_C() * (d2u_dx2 + d2u_dy2) * TIMESTEP;
+        bob_arr[y][x].u_t *= 0.999;
+      }
+    }
+    
+    for (const row of bob_arr)
+      for (const bob of row)
+        bob.u += bob.u_t * TIMESTEP;
+  }
+  
+  for (let y = 0; y < BOB_NUM - 1; y++) {
+    for (let x = 0; x < BOB_NUM - 1; x++) {
+      const a = new vec3_t(x, bob_arr[y][x].u, y);
+      const b = new vec3_t(x + 1, bob_arr[y][x + 1].u, y);
+      const c = new vec3_t(x, bob_arr[y + 1][x].u, y + 1);
+      const d = new vec3_t(x + 1, bob_arr[y + 1][x + 1].u, y + 1);
+      
+      pen3d.circle(a, 0.1);
+      pen3d.line(a, b);
+      pen3d.line(a, c);
+    }
+  }
+  
+  for (const sphere of sphere_arr)
+    sphere.draw();
+
+  pen.stroke();
+}
+
+function free_move()
+{
+  let move_dir = new vec3_t();
+  
+  if (input.get_key(key.code("W")))
+    move_dir = move_dir.add(new vec3_t(0, 0, +1));
+  if (input.get_key(key.code("A")))
+    move_dir = move_dir.add(new vec3_t(-1, 0, 0));
+  if (input.get_key(key.code("S")))
+    move_dir = move_dir.add(new vec3_t(0, 0, -1));
+  if (input.get_key(key.code("D")))
+    move_dir = move_dir.add(new vec3_t(+1, 0, 0));
+  
+  camera.pos = camera.pos.add(move_dir.rotate_zxy(camera.rot).mulf(5 * TIMESTEP));
+}
+
+function free_look()
+{
+  const sensitivity = 0.6;
+  
+  if (input.lock_status()) {
+    camera.rot.x = -input.get_mouse_pos().y * 2 * Math.PI * sensitivity;
+    camera.rot.y = -input.get_mouse_pos().x * 2 * Math.PI * sensitivity;
+  } else if (input.get_mouse_down()) {
+    input.lock();
+  }
+}
+
+function reset()
+{
+  sphere_arr.length = 0;
+  for (let i = 0; i < 4; i++) {
+    const pos = new vec3_t((rand() + 0.5) * BOB_NUM, 3, (rand() + 0.5) * BOB_NUM);
+    sphere_arr.push(new sphere_t(pos, new vec3_t()));
+  }
+  
+  bob_arr.length = 0;
+  
+  for (let i = 0; i < BOB_NUM; i++) {
+    const row = [];
+    for (let j = 0; j < BOB_NUM; j++)
+      row.push(new bob_t(0.0, 0.0));
+    
+    bob_arr.push(row);
+  }
+}
+
+class sphere_t {
+  constructor(pos, vel)
+  {
+    this.pos = pos;
+    this.vel = vel;
+  }
+  
+  apply_gravity()
+  {
+    this.vel.y -= 20 * TIMESTEP;
+  }
+  
+  update()
+  {
+    this.apply_gravity();
+    this.integrate();
+    this.clip_wave();
+    this.clip_bound();
+  }
+  
+  clip_bound()
+  {
+    if (this.pos.x - RADIUS < 0) {
+      this.pos.x = RADIUS;
+      this.vel.x = 0;
+    }
+    
+    if (this.pos.x + RADIUS >= BOB_NUM) {
+      this.pos.x = BOB_NUM - RADIUS;
+      this.vel.x = 0;
+    }
+    
+    if (this.pos.z - RADIUS < 0) {
+      this.pos.z = RADIUS;
+      this.vel.z = 0;
+    }
+    
+    if (this.pos.z + RADIUS >= BOB_NUM) {
+      this.pos.z = BOB_NUM - RADIUS;
+      this.vel.z = 0;
+    }
+  }
+  
+  clip_wave()
+  {
+    const x = clamp(Math.floor(this.pos.x), 1, BOB_NUM - 2);
+    const y = clamp(Math.floor(this.pos.z), 1, BOB_NUM - 2);
+    
+    const a = new vec3_t(x, bob_arr[y][x].u, y);
+    const b = new vec3_t(x + 1, bob_arr[y][x + 1].u, y);
+    const c = new vec3_t(x, bob_arr[y + 1][x].u, y + 1);
+    
+    const ab = b.sub(a);
+    const ac = c.sub(a);
+    
+    const n = ac.cross(ab);
+    const d = a.dot(n);
+    
+    const p_d = this.pos.dot(n) - d - RADIUS;
+    
+    if (p_d < 0) {
+      const beta = p_d * 0.1 / TIMESTEP;
+      const lambda = -(n.dot(this.vel) + beta);
+      const d_v = n.mulf(lambda);
+      this.vel = this.vel.add(d_v);
+    }
+  }
+  
+  integrate()
+  {
+    this.pos = this.pos.add(this.vel.mulf(TIMESTEP));
+  }
+  
+  draw()
+  {
+    const p_x = new vec3_t(1, 0, 0);
+    const p_y = new vec3_t(0, 1, 0);
+    const p_z = new vec3_t(0, 0, 1);
+    
+    const x_a = this.pos.add(p_x);
+    const x_b = this.pos.sub(p_x);
+    
+    const y_a = this.pos.add(p_y);
+    const y_b = this.pos.sub(p_y);
+    
+    const z_a = this.pos.add(p_z);
+    const z_b = this.pos.sub(p_z);
+    
+    pen3d.circle(this.pos, 1);
+    pen3d.line(x_a, x_b);
+    pen3d.line(y_a, y_b);
+    pen3d.line(z_a, z_b);
+  }
+};
+
+start();
+
+setInterval(function() {
+  update();  
+}, TIMESTEP * 1000);
+
+document.getElementById("stop").addEventListener("click", () => stop = true);
+document.getElementById("start").addEventListener("click", () => stop = false);
+document.getElementById("reset").addEventListener("click", reset);
+
